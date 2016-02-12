2b6421cc9c4e6d887481c6f57668df162d2e89db
ALRW <awerner1@googlemail.com>
2016-02-12 11:42:54 +0000

    refactored feature tests

--- test/e2e/angularBloggerFeature.js
+++ test/e2e/angularBloggerFeature.js
@@ -5,12 +5,6 @@ describe('AngularBlogger', function() {
     expect(browser.getTitle()).toEqual('Angular Blogger');
   });
 
-  it('shows posts that are present on the site', function() {
-    expect(element.all(by.css(".publishedTitle")).first().getText()).toEqual("Title: My new post");
-    expect(element.all(by.css(".publishedAuthor")).first().getText()).toEqual("Author: User1");
-    expect(element.all(by.css(".publishedDescription")).first().getText()).toEqual("Description: A very new post");
-  });
-
   it('can add new posts to the site', function() {
     element.all(by.css('li')).first().click();
     expect(element(by.css('h3')).getText()).toEqual('Create a New Post');
@@ -20,8 +14,8 @@ describe('AngularBlogger', function() {
     element(by.css('button')).click();
     element.all(by.css('a')).first().click();
     expect(browser.getCurrentUrl()).toBe('http://localhost:3000/#/home');
-    expect(element.all(by.css('.publishedTitle')).get(1).getText()).toEqual('Title: Great Post');
-    expect(element.all(by.css('.publishedDescription')).get(1).getText()).toEqual('Description: This is a great Post');
+    expect(element.all(by.css('.publishedTitle')).get(0).getText()).toEqual('Title: Great Post');
+    expect(element.all(by.css('.publishedDescription')).get(0).getText()).toEqual('Description: This is a great Post');
   });
 
   it('throws an error if I try to add an empty post', function() {
